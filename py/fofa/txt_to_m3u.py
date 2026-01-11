b6364bf631d6df7c65653bc8283a6d1e243fdb17
linyu345 <linyu345@outlook.com>
2026-01-11 21:44:11 +0800

    Update txt_to_m3u.py

--- py/fofa/txt_to_m3u.py
+++ py/fofa/txt_to_m3u.py
@@ -3,8 +3,8 @@ import re
 import requests
 
 # 输入输出文件（改为远程 GitHub Raw URL）
-INPUT_URL = "https://raw.githubusercontent.com/linyu345/2026/refs/heads/main/py/fofa/IPTV.txt"
-OUTPUT_FILE = "IPTV.m3u"
+INPUT_URL = "py/fofa/IPTV.txt"
+OUTPUT_FILE = "py/fofa/IPTV.m3u"
 
 # 台标和 EPG（保持不变）
 LOGO_BASE = "https://gcore.jsdelivr.net/gh/kenye201/TVlog/img/"
